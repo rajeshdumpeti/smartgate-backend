8827c5686c5c4b64475bc3ba981b9be35d8a990e
rajeshdumpeti <dumpetirajesh123@gmail.com>
2025-10-26 06:43:36 -0400

    feat: add Student model and initial Alembic migration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.routers import student, event
+from app.core.db import engine
+from app.models import student as student_model, event as event_model
+from app.core.db import engine
 
-app = FastAPI(title="SmartGate API", version="1.0.0")
+app = FastAPI(title="SmartGate Backend", version="1.0")
 
 # CORS for local frontend
 app.add_middleware(
@@ -12,6 +16,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# # Create tables if not exist
+# student_model.Base.metadata.create_all(bind=engine)
+# event_model.Base.metadata.create_all(bind=engine)
+
+# app.include_router(student.router, prefix="/api/v1/students", tags=["Students"])
+# app.include_router(event.router, prefix="/api/v1/events", tags=["Events"])
+
+
 @app.get("/")
 def root():
-    return {"message": "SmartGate API running successfully 🚀"}
+    return {"message": "SmartGate Backend running successfully 🚀"}
+
+
+
+@app.get("/db-check")
+def check_db():
+    try:
+        with engine.connect() as conn:
+            return {"status": "✅ Database connected successfully!"}
+    except Exception as e:
+        return {"error": str(e)}
